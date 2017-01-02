b0fb74f48add77d64c2749ab790ba2f6aa816acb
Rickard Lindberg <ricli85@gmail.com>
2017-01-02 09:25:53 +0100

    Remove encoding module.

--- selectlib/encoding.py
+++
@@ -1,8 +0,0 @@
-import locale
-
-
-def to_unicode(binary, fail=False):
-    if fail:
-        return binary.decode(locale.getpreferredencoding())
-    else:
-        return binary.decode(locale.getpreferredencoding(), "replace")
